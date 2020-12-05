88b315f1b993e7c4d15a57a65048e4e9e36c4c1c
RafaF196 <RafaF196@gmail.com>
2020-12-05 17:39:51 +0100

    Video 12

---
+++ Assets/PathFinder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFinder : MonoBehaviour
+{
+    [SerializeField] WayPoint startWayPoint, endWayPoint;
+
+    Dictionary<Vector2Int, WayPoint> grid = new Dictionary<Vector2Int, WayPoint>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadBlocks();
+        ColorStartAndEnd();
+    }
+
+    private void ColorStartAndEnd()
+    {
+        startWayPoint.SetTopColor(Color.green);
+        endWayPoint.SetTopColor(Color.red);
+    }
+
+    private void LoadBlocks()
+    {
+        var waypoints = FindObjectsOfType<WayPoint>();
+        foreach (WayPoint waypoint in waypoints)
+        {
+            var gridPos = waypoint.GetGridPos();
+            if (grid.ContainsKey(gridPos))
+            {
+                Debug.LogWarning("Skipping overlapping block " + waypoint);
+            }
+            else
+            {
+                grid.Add(gridPos, waypoint);
+            }
+        }
+    }
+}
