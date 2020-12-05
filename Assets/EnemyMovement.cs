72b02d98372ef085af45af6b234a7140687a2dcf
RafaF196 <RafaF196@gmail.com>
2020-12-05 16:19:55 +0100

    Video 8

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -5,25 +5,20 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField] List<Block> path;
+    [SerializeField] List<WayPoint> path;
 
     // Start is called before the first frame update
     void Start()
     {
-        PrintAllWaypoints();
+        StartCoroutine(FollowPath());
     }
 
-    private void PrintAllWaypoints()
+    private IEnumerator FollowPath()
     {
-        foreach (Block waypoint in path)
+        foreach (WayPoint waypoint in path)
         {
-            print(waypoint.name);
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(1f);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
