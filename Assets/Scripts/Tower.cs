11bd140bfbdd9ad40e9bf518fa3a78303eb714b6
RafaF196 <RafaF196@gmail.com>
2020-12-08 11:31:57 +0100

    Video 27

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -6,14 +6,16 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     [SerializeField] Transform objectToPan;
-    [SerializeField] Transform targetEnemy;
-
+    
     [SerializeField] float attackRange = 50f;
     [SerializeField] ParticleSystem projectileParticle;
+
+    Transform targetEnemy;
 
     // Update is called once per frame
     void Update()
     {
+        SetTargetEnemy();
         if (targetEnemy)
         {
             objectToPan.LookAt(targetEnemy);
@@ -23,6 +25,31 @@ public class Tower : MonoBehaviour
         {
             Shoot(false);
         }
+    }
+
+    private void SetTargetEnemy()
+    {
+        var sceneEnemies = FindObjectsOfType<EnemyDamage>();
+        if (sceneEnemies.Length == 0) { return; }
+        Transform closestEnemy = sceneEnemies[0].transform;
+
+        foreach (EnemyDamage enemy in sceneEnemies)
+        {
+            closestEnemy = GetClosest(closestEnemy, enemy.transform);
+        }
+
+        targetEnemy = closestEnemy;
+    }
+
+    private Transform GetClosest(Transform transformA, Transform transformB)
+    {
+        var distToA = Vector3.Distance(transform.position, transformA.position);
+        var distToB = Vector3.Distance(transform.position, transformB.position);
+        if (distToA < distToB)
+        {
+            return transformA;
+        }
+        return transformB;
     }
 
     private void FireAtEnemy()
