a809c909e1a63dcf081bc2eb1c57df754ded4fe9
RafaF196 <RafaF196@gmail.com>
2020-12-08 11:00:14 +0100

    Video 24

--- Assets/Scripts/EnemyDamage.cs
+++ Assets/Scripts/EnemyDamage.cs
@@ -1,9 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyDamage : MonoBehaviour
 {
+    [SerializeField] int hitPoints = 5;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,5 +16,20 @@ public class EnemyDamage : MonoBehaviour
     private void OnParticleCollision(GameObject other)
     {
         print("I'm hit!");
+        ProcessHit();
+        if (hitPoints < 1)
+        {
+            killEnemy();
+        }
+    }
+
+    private void ProcessHit()
+    {
+        hitPoints--;
+    }
+
+    private void killEnemy()
+    {
+        Destroy(gameObject);
     }
 }
